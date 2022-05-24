3ea6b030765d066c7811b492a5e08e2ed0b3242b
kelvinLin9 <kelvin80121@hotmail.com>
2022-05-24 09:30:01 +0800

    git init

--- src/router/index.js
+++ src/router/index.js
@@ -14,6 +14,46 @@ const routes = [
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
     component: () => import(/* webpackChunkName: "about" */ '../views/AboutView.vue')
+  },
+  {
+    path: '/login',
+    component: () => import('../views/LoginView.vue')
+  },
+  {
+    path: '/dashboard',
+    component: () => import('../views/DashboardView.vue'),
+    children: [
+      {
+        path: 'products',
+        component: () => import('../views/ProductsView.vue')
+      },
+      {
+        path: 'orders',
+        component: () => import('../views/Orders.vue')
+      },
+      {
+        path: 'coupons',
+        component: () => import('../views/Coupons.vue')
+      }
+    ]
+  },
+  {
+    path: '/user',
+    component: () => import('../views/Userboard.vue'),
+    children: [
+      {
+        path: 'cart',
+        component: () => import('../views/UserCart.vue')
+      },
+      {
+        path: 'product/:productId',
+        component: () => import('../views/UserProduct.vue')
+      },
+      {
+        path: 'checkout/:orderId',
+        component: () => import('../views/UserCheckout.vue')
+      }
+    ]
   }
 ]
 
--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,6 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
-  transpileDependencies: true
+  transpileDependencies: true,
+  // 修正檔案命名問題
+  lintOnSave: false
 })
